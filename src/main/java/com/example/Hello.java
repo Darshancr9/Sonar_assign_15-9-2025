01e8f7b546d931eef30ddf0b270eaf64d6490045
Ubuntu <ubuntu@ip-172-31-25-231.ec2.internal>
2025-09-15 20:36:05 +0000

    added src

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, SonarQube!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
